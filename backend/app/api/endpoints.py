0aa559c45d01323affbf113ed17e38bd991790df
Victor Pla <77746818+VictorPlaSanchis@users.noreply.github.com>
2025-07-30 14:09:00 +0200

    corrector esgarrifal

--- backend/app/api/endpoints.py
+++ backend/app/api/endpoints.py
@@ -7,5 +7,5 @@ router = APIRouter()
 async def analyze_dataset(file: UploadFile = File(...)):
     df = pd.read_csv(file.file)
     preview = df.head(3).to_dict(orient="records")
-    columns = df.columns.tolis()
+    columns = df.columns.tolist()
     return {"columns": columns, "preview": preview}
